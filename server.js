695d37307a93b8c5100bcc476a200d8647bb6cfe
Andy Pham <apham3x@yahoo.com>
2021-01-18 15:32:03 -0500

    Client renders base react component, started a testing folder

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const port = '8080';
-const faker = require('faker');
+const port = '5555';
 const db = require('./server/database/db.js');
 
-app.use(express.static('client'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,15 +15,41 @@ app.get('/:sort/:productId', (req, res) => {
   var sortBy = req.params.sort;
   var productId = Number(req.params.productId);
   // Will sort by default, best, or new with the productId that equals the productId coming in
-  db.grab[sortBy](productId)
-    .then((results) => {
-      res.json(results);
-    })
-    .catch((err) => {
-      if (err) {
-        console.error(err);
-      }
-    })
+  if (sortBy === 'default') {
+    db.grab.default(productId)
+      .then((results) => {
+        res.json(results);
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        }
+      })
+  };
+
+  if (sortBy === 'best') {
+    db.grab.best(productId)
+      .then((results) => {
+        res.json(results);
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        }
+      })
+  };
+
+  if (sortBy === 'new') {
+    db.grab.new(productId)
+      .then((results) => {
+        res.json(results);
+      })
+      .catch((err) => {
+        if (err) {
+          console.error(err);
+        }
+      })
+  }
 });
 
 
